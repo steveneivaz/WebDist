e1e0df9022f4dbba411326667fedb085292efb6d
Steven <steveneivaz@lewisu.edu>
2019-11-17 21:51:17 -0600

    '

--- Mustang-V2/Main.js
+++ Mustang-V2/Main.js
@@ -4,6 +4,7 @@ var contactURLArray = [];
 var contactArray = [];
 var loadingContact = 0;
 var currentContactIndex = 0;
+var inp;
 
 button.addEventListener("click", function(){
 	var Request = new XMLHttpRequest();
@@ -31,25 +32,22 @@ var contactURLArray = [];
 var contactArray = [];
 var loadingContact = 0;
 
+
 function loadIndex() {
-    var Request = new XMLHttpRequest();
-	var htmlString = "";
-    Request.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
-    Request.onload = function() {
-        console.log("Index JSON:" + Request.responseText);
-        document.getElementById("indexID").innerHTML = Request.responseText;
-        contactIndex = JSON.parse(Request.responseText);
-		
-	
+    
+    var indexRequest = new XMLHttpRequest();
+    indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
+    indexRequest.onload = function() {
+        console.log("Index JSON:" + indexRequest.responseText);
+        document.getElementById("indexID").innerHTML = indexRequest.responseText;
+        contactIndex = JSON.parse(indexRequest.responseText);
         for (i=0; i<contactIndex.length; i++) {
             contactURLArray.push(contactIndex[i].ContactURL);
-			
-			
         }
         console.log("ContactURLArray: " + JSON.stringify(contactURLArray));
-	
+        loadContacts();
     }
-    Request.send();
+    indexRequest.send();
 }
 
 function loadContacts() {
@@ -98,7 +96,7 @@ function viewCurrentContact() {
     document.getElementById("stateID").value = currentContact.state;
     document.getElementById("zipID").value = currentContact.zip;  
 
-    // Todo: Add additional fields.
+    
     document.getElementById("statusID").innerHTML = "Status: Viewing contact " + (currentContactIndex+1) + " of " + contactArray.length;
 }
 
@@ -119,37 +117,32 @@ function next() {
     currentContact = contactArray[currentContactIndex];
     viewCurrentContact();
     
-    // Todo: Disable next button when there is no next item.
-    // Todo: Save changed items to contacts array and resort array.
+    
 }
 
 function add() {
     console.log('add()');
 
-    // Todo: Implement add functionality by inserting new element into array.
+   	newcontact= contactArray[currentContactIndex];
+	console.log(newcontact);
+	document.getElementById("nameID").value = newcontact.preferredName;   
+    document.getElementById("emailID").value = newcontact.email;   
+    document.getElementById("cityID").value = newcontact.city;   
+    document.getElementById("stateID").value = newcontact.state;
+    document.getElementById("zipID").value = newcontact.zip;
 }
 
 function remove() {
     console.log('remove()');
 
-    // Todo: Implement delete functionality by deleting element from array.
+    
 }
 
-function zipFocusFunction() {
-    console.log('focusFunction()');
-
-    // Todo: Remove the function as it is not needed.
-}
 
 function zipBlurFunction() {
     getPlace();
 }
 
-function keyPressed() {
-    console.log('keyPressed()');
-
-    // This type of function should be useful in search as it implements keyPressed.
-}
 
 function getPlace() {
     var zip = document.getElementById("zipID").value
@@ -158,7 +151,7 @@ function getPlace() {
     console.log("function getPlace(zip) { ... }");
     var xhr = new XMLHttpRequest();
 
-    // Register the embedded handler function
+  
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
             var result = xhr.responseText;
@@ -179,36 +172,84 @@ function initApplication() {
     loadIndex();
 }
 
-function loadIndex() {
-    // Load the Mustang index file.
-    var indexRequest = new XMLHttpRequest();
-    indexRequest.open('GET', 'https://mustang-index.azurewebsites.net/index.json');
-    indexRequest.onload = function() {
-        console.log("Index JSON:" + indexRequest.responseText);
-        document.getElementById("indexID").innerHTML = indexRequest.responseText;
-        contactIndex = JSON.parse(indexRequest.responseText);
-        for (i=0; i<contactIndex.length; i++) {
-            contactURLArray.push(contactIndex[i].ContactURL);
-        }
-        console.log("ContactURLArray: " + JSON.stringify(contactURLArray));
-        loadContacts();
-    }
-    indexRequest.send();
-}
+
 
 function loadContacts() {
-    // Clear the current contactArray.
+   
     contactArray.length = 0;
     loadingContact = 0;
 
-    // Note that W3C documentation and my experimentation indicate that each XMLHttpRequest callback function must be a 
-    // unique instance of a function. A better implmentation would have had an array of callback functions instead of a 
-    // recursive call to load
+   
     if (contactURLArray.length > loadingContact) {
         loadNextContact(contactURLArray[loadingContact]);
     }
 }
-
+function autocomplete(inp, getCityState) {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+       closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+     a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      this.parentNode.appendChild(a);
+       for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.addEventListener("click", function(e) {
+             inp.value = this.getElementsByTagName("input")[0].value;
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        currentFocus++;
+         addActive(x);
+      } else if (e.keyCode == 38) { //up
+        currentFocus--;
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentFocus > -1) {
+         if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+   for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+ document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
 function loadNextContact(URL) {
     console.log("URL: " + URL);
     contactRequest = new XMLHttpRequest();
@@ -233,7 +274,7 @@ function loadNextContact(URL) {
             viewCurrentContact()
             console.log(contactArray);
 
-            //Todo: Sort contacts array.
+            
         }
     }
 
