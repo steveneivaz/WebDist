c5a7947343da9b091876cac15a9d0d1097f2884a
Steven <steveneivaz@lewisu.edu>
2019-12-05 11:36:13 -0600

    ....

--- Final Project/PackResults.php
+++ Final Project/PackResults.php
@@ -46,74 +46,23 @@
 </script>
 									   
 <?php
-		echo '<script type="text/javascript">',
-     'validate();',
-     '</script>'
-;
-      $pack = $_POST["enter"];
-			 
-									   
-      $pack = addslashes($pack);                                
-      @ $db = new mysqli("localhost", "phpuser2", "1425", "innistrad3.0");
-
-        if($db->connect_error) {
-            die("Connect error $db->connect_errno : $db->connecterror");
-        }
-
-        echo "Successful login.<br/>";
-		for ($k = 0 ; $k < $pack; $k++){ 
-        $query = "SELECT * FROM ncards order by RAND() LIMIT 10" ;
-         if($results = $db->query($query)) {
-            $num_fields = $results->field_count;
-            while($row = $results->fetch_row()) {
-                echo "<tr>";
-                for($i=0; $i<$num_fields; $i++) {
-                    echo "<td>".stripslashes($row[$i])."</td>";
-                }
-                echo "<tr/>";
-            }
-            $results->free();
-        }
-			 $query1 = "SELECT * FROM nuncom order by RAND() LIMIT 3" ;
-         if($results = $db->query($query1)) {
-            $num_fields = $results->field_count;
-            while($row = $results->fetch_row()) {
-                echo "<tr>";
-                for($i=0; $i<$num_fields; $i++) {
-                    echo "<td>".stripslashes($row[$i])."</td>";
-                }
-                echo "<tr/>";
-            }
-            $results->free();
-        }
-			 $query2 = "SELECT * FROM nrm order by RAND() LIMIT 1" ;
-         if($results = $db->query($query2)) {
-            $num_fields = $results->field_count;
-            while($row = $results->fetch_row()) {
-                echo "<tr>";
-                for($i=0; $i<$num_fields; $i++) {
-                    echo "<td>".stripslashes($row[$i])."</td>";
-                }
-                echo "<tr/>";
-            }
-            $results->free();
-        }
-		}
-        $db->close();
-   
-    ;
-    
-    echo "<p> You opened $pack pack(s) </p>";
-        
-		
-		
-		if(!$pack){
-			echo("Have not entered");
-			echo '<a href="../HTML/PackOpener.html"> Open another pack </a>';
-			exit;
-		}
-	
-	
+    $serverName = "dokkan.database.windows.net"; 
+    $connectionOptions = array(
+        "Database" => "your_database", 
+        "Uid" => "steveneivaz", 
+        "PWD" => "S13421963e"
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT TOP (7) * FROM [dbo].[Ncards]";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
 ?>
 <br/>
                                        
